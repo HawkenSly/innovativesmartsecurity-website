81a7029a1c67e20db3297b33055af054e682c550
Hawken <HawkenSly@gmail.com>
2022-07-26 13:17:08 -0400

    updates

--- index.e5ac7667.js
+++ index.e5ac7667.js
@@ -533,6 +533,22 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"aitAD":[function(require,module,exports) {
 var _bootstrap = require("bootstrap");
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 401) {
+            document.getElementById("navbar_top").classList.add("sillycolor");
+            document.getElementById("brandcolor").classList.add("text-white");
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector(".navbar").offsetHeight;
+            document.body.style.paddingTop = navbar_height + "px";
+        } else {
+            document.getElementById("navbar_top").classList.remove("sillycolor");
+            document.getElementById("brandcolor").classList.remove("text-white");
+            // remove padding top from body
+            document.body.style.paddingTop = "0";
+        }
+    });
+});
 
 },{"bootstrap":"h36JB"}],"h36JB":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
