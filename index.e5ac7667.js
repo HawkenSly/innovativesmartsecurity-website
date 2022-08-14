cfcffe5b8fd92e25ac8f0f1619b9c5bd2414ecf4
Hawken <HawkenSly@gmail.com>
2022-08-14 04:43:57 -0400

    Update to site

--- index.e5ac7667.js
+++ index.e5ac7667.js
@@ -537,18 +537,20 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("scroll", function() {
         if (window.scrollY > 401) {
             document.getElementById("navbar_top").classList.add("sillycolor");
-            document.getElementById("brandcolor").classList.add("text-white");
             // add padding top to show content behind navbar
             navbar_height = document.querySelector(".navbar").offsetHeight;
-            document.body.style.paddingTop = navbar_height + "px";
         } else {
             document.getElementById("navbar_top").classList.remove("sillycolor");
-            document.getElementById("brandcolor").classList.remove("text-white");
             // remove padding top from body
             document.body.style.paddingTop = "0";
         }
     });
 });
+const hero2 = document.querySelectorAll(".hero2");
+const observer = new IntersectionObserver((entries)=>{
+    console.log(entries);
+});
+observer.observe(hero2[0]);
 
 },{"bootstrap":"h36JB"}],"h36JB":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
